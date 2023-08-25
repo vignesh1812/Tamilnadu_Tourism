fa033e2b9d89cbc532e7d6fa829ebaa7026155cd
Vigneshwaran M <139222496+vignesh1812@users.noreply.github.com>
2023-08-25 17:50:35 +0530

    Update App.js
    new pagevisit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
+import Placevisit from './Placevisit';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-         <Navbar/>
-         <Home/>
-    </div>
+     <BrowserRouter>
+      <div className="App ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route  path="/places" element={<Placevisit/>}/>
+        </Routes>
+      </div>
+     </BrowserRouter>
+     
+    
   );
 }
 
