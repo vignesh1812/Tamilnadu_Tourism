2f0c28dca89775ff57fd7a7326074e50aec8771e
Vigneshwaran M <139222496+vignesh1812@users.noreply.github.com>
2023-08-25 18:46:15 +0530

    nopage found
    asdf

---
+++ src/Nopage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nopage = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center  h-100 w-100 flex-column'>
+    <h1>Error 404</h1>
+    <h4>Page Not Found!</h4>
+
+</div>  )
+}
+
+export default Nopage
