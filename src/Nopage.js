4f26abb6a282de761c127f295864046f9cb25268
Vigneshwaran M <139222496+vignesh1812@users.noreply.github.com>
2023-08-26 14:45:11 +0530

    Update Nopage.js
    center content

--- src/Nopage.js
+++ src/Nopage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nopage = () => {
   return (
-    <div className='d-flex justify-content-center align-items-center  h-100 w-100 flex-column'>
+    <div className='d-flex justify-content-center align-items-center flex-column' style={{marginTop:200}}>
     <h1>Error 404</h1>
     <h4>Page Not Found!</h4>
 
