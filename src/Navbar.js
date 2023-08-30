9f34f7474e9b0a021be90cc306b8c92bf560e87f
Vigneshwaran M <139222496+vignesh1812@users.noreply.github.com>
2023-08-30 21:29:55 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,36 +2,36 @@ import React from 'react'
 import { BiMenu } from "react-icons/bi";
 import './Navbar.css';
 
-// import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink as Link } from 'react-router-hash-link';
 
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg shadow-sm" id='color' >
-                <div class="container">
-                    <a class="navbar-brand text-uppercase fw-bold text-white" href="/">Tamilnadu Tourism</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon display-5 p-0 text-white"><BiMenu/></span>
+            <nav className="navbar navbar-expand-lg shadow-sm" id='color' >
+                <div className="container">
+                    <a className="navbar-brand text-uppercase fw-bold text-white" href="/">Tamilnadu Tourism</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="display-5 pb-2 text-white" id='icons' ><BiMenu/></span>
                     </button>
-                    <div class="collapse navbar-collapse  " id="navbarNav">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item fw-bold ">
-                                <a class="nav-link text-white my-1 mx-2 " aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse  " id="navbarNav">
+                        <ul className="navbar-nav ms-auto text-center">
+                            <li className="nav-item fw-bold ">
+                                <Link className="nav-link text-white my-1 mx-2 " aria-current="page" to='/' >Home</Link>
                             </li>
-                            <li class="nav-item fw-bold ">
-                                <a class="nav-link text-white my-1 mx-2 " href="/">Places to Visit</a>
+                            <li className="nav-item fw-bold ">
+                                <Link className="nav-link text-white my-1 mx-2 "to='/places'>Places to Visit</Link>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link text-white my-1 mx-2 text-capitalize" href="/">Best Time to Visit</a>
+                            <li className="nav-item fw-bold">
+                                <Link className="nav-link text-white my-1 mx-2 text-capitalize" to='/time'>Best Times to Visit</Link>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link text-white my-1 mx-2 text-capitalize" href="/" tabindex="-1" aria-disabled="true">packages</a>
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link text-white my-1 mx-2 text-capitalize" href="/" tabIndex="-1" aria-disabled="true">packages</a>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link text-white my-1 mx-2 text-capitalize" href="/" tabindex="-1" aria-disabled="true">destination</a>
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link text-white my-1 mx-2 text-capitalize" href="/" tabIndex="-1" aria-disabled="true">destination</a>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link my-1 mx-2 text-white text-capitalize" href="/" tabindex="-1" aria-disabled="true">food</a>
+                            <li className="nav-item fw-bold">
+                            <Link className="nav-link text-white my-1 mx-2 text-capitalize" to='/food'>Food</Link>
                             </li>
                         </ul>
                     </div>
